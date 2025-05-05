6d346ff4023b22e89aff8d03ef1e9d7cd1223564
Nagodo <89977444+Nagodo@users.noreply.github.com>
2025-05-05 10:51:08 +0200

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-
-function Homepage() {
-    return(
-        <div>
-            <h1 className="text-red-600">Hello react in electrodwdwn</h1>
-            <h1 className="red">Homepage</h1>
-        </div>
-    )
-}
+import Homepage from './pages/HomePage/page';
 
 export default function App() {
     return (
---
+++ src/pages/HomePage/page.tsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+    return(
+        <div>
+            <h1 className="text-red-600">Hello react in electrodwdwn</h1>
+            <h1 className="red">Homepage</h1>
+        </div>
+    )
+}
